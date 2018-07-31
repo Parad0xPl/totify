d8dbbafbd64b9a894d083913ba4b4dfb22ff3086
Parad0x <paradxpl@gmail.com>
2018-07-31 12:53:29 +0200

    - Add escaping string

--- src/utils/countAndSlice.js
+++ src/utils/countAndSlice.js
@@ -7,15 +7,21 @@
  * @param {string} [separator=";"] Separator to split and count it`s occurance 
  * @returns {[string[], number]}
  */
-function countAndSlice(input: string, separator: string = ";"): [string[], number] {
+function countAndSlice(input: string, separator: string = ";", escapeCharacter: string = "\\"): [string[], number] {
   let buff: string = "";
   let arr: string[] = [];
   let counter: number = 0;
+  let escape = false;
   for (let char of input) {
-    if (char == separator) {
+    if (escape) {
+      buff = buff.concat(char);
+      escape = false;
+    } else if (char == separator) {
       counter++;
       arr.push(buff);
       buff = "";
+    } else if (char == escapeCharacter) {
+      escape = true;
     } else {
       buff = buff.concat(char);
     }
